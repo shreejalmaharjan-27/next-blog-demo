5c2d6369872fed47986f91333d38711a12e66866
shreejalmaharjan-27 <shreejal@rafled.com>
2023-09-09 14:00:15 +0000

    general update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-import DarkModeToggle from '@/components/DarkModeToggle'
-import '../styles/globals.css'
+import DarkModeToggle from "@/components/DarkModeToggle";
+import "../styles/globals.css";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <div className="min-h-screen bg-slate-100 dark:bg-slate-900 text-black dark:text-white">
-          <div className='container mx-auto py-2'>
+          <div className="container mx-auto py-2">
             <div className="inline-flex justify-between items-center w-full  mb-6">
-              <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">My Blog</h2>
+              <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">
+                My Blog
+              </h2>
               <DarkModeToggle />
             </div>
             {children}
@@ -19,5 +21,5 @@ export default function RootLayout({
         </div>
       </body>
     </html>
-  )
+  );
 }
